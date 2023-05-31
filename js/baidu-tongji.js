69a384327b68df2fa14d0f64317d5bad1b0824ce
phoenixmerk <yaojunyimerk11@163.com>
2023-05-31 18:59:23 +0800

    Site updated: 2023-05-31 18:59:23

--- js/baidu-tongji.js
+++ js/baidu-tongji.js
@@ -1,7 +1,10 @@
+<script>
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?";
+  hm.src = "https://hm.baidu.com/hm.js?b722cfdcbb5d8e344a0ff5e3c85820d6";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
+</script>
+
