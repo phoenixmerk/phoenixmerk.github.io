ab4a5a625190922cb535bdbddb38856107720349
phoenixmerk <785415176@qq.com>
2023-09-01 22:07:01 +0800

    Site updated: 2023-09-01 22:07:01

---
+++ js/baidu-tongji.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?b722cfdcbb5d8e344a0ff5e3c85820d6";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
